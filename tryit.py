9c3f32d6d1df7e33ed858334d8e5f17d13dcad67
shispt <shispt18@gmail.com>
2016-07-09 21:26:35 +0800

    add argparse

--- tryit.py
+++ tryit.py
@@ -1,8 +1,11 @@
 #!/usr/bin/env python
 # -*- coding: utf-8 -*-
 
+from os import path
 import sys
 import re
+import argparse
+
 from wordcloud import WordCloud
 import jieba
 import requests
@@ -16,29 +19,39 @@ def read_stopwords(file):
                 continue
             stopwords.add(line.strip().decode('utf-8'))
     return stopwords
+stopwords = read_stopwords(path.join(path.dirname(__file__), 'stopwords.txt'))
 
-stopwords = read_stopwords('stopwords.txt')
+def main():
+    parser = argparse.ArgumentParser()
+    parser.add_argument('--url', metavar='URL', required=True, help='input the url')
+    args = parser.parse_args()
 
-url = sys.argv[1]
-ret = requests.get(url)
-ret.encoding = ret.apparent_encoding
-origin_text = ret.text
-origin_text = re.sub(ur'<script.*?>.*?</script>', '', origin_text, flags=re.I|re.M|re.DOTALL)
-origin_text = re.sub(ur'<style.*?>.*?</style>', '', origin_text, flags=re.I|re.M|re.DOTALL)
+    output_file = path.join(path.dirname(__file__), 'wordcloud.png')
 
-doc = html.fromstring(origin_text)
-text = doc.xpath('//body//text()')
-text = [i.strip() for i in text if i.strip()]
-text = u' '.join(text)
+    url = args.url
 
-seg = jieba.cut(text)
-seg = [i.strip() for i in seg if i.strip() and not i.strip().isdigit() and i.strip() not in stopwords]
-seg = u' '.join(seg)
+    response = requests.get(url, timeout=15)
+    response.encoding = response.apparent_encoding
 
-wordcloud = WordCloud(font_path='simhei.ttf', background_color='black', margin=5, width=1800, height=800)
-wordcloud = wordcloud.generate(seg)
-image = wordcloud.to_image()
-with open('wordcloud.png', 'wb') as f:
-    image.save(f, format='png')
+    origin_text = response.text
+    origin_text = re.sub(ur'<script.*?>.*?</script>', '', origin_text, flags=re.I|re.M|re.DOTALL)
+    origin_text = re.sub(ur'<style.*?>.*?</style>', '', origin_text, flags=re.I|re.M|re.DOTALL)
 
+    doc = html.fromstring(origin_text)
+    text = doc.xpath('//body//text()')
+    text = [i.strip() for i in text if i.strip()]
+    text = u' '.join(text)
 
+    seg = jieba.cut(text)
+    seg = [i.strip() for i in seg if i.strip() and not i.strip().isdigit() and i.strip() not in stopwords]
+    seg = u' '.join(seg)
+
+    wordcloud = WordCloud(font_path='simhei.ttf', background_color='black', margin=5, width=1800, height=800)
+    wordcloud = wordcloud.generate(seg)
+    image = wordcloud.to_image()
+
+    with open(output_file, 'wb') as f:
+        image.save(f, format='png')
+
+if __name__ == '__main__':
+    main()
